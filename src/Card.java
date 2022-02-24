b4db2a16e15eb8176c81cc444344062fa9a471d6
ChrisAE123 <magnuschase145@gmail.com>
2022-02-24 09:14:35 -0600

    Changes

--- src/Card.java
+++ src/Card.java
@@ -1,28 +1,63 @@
 public class Card {
+
     // PROPERTIES
     private String color;
-    private char rank;
+    private String rank;
+    private String symbol;
+    private int value;
+    private  boolean playable;
 
     // CONSTRUCTORS
-    public Card() {}
+    public Card() {
+        this.color="";
+        this.rank="";
+        this.symbol="";
+        this.value=0;
+        this.playable=false;
+    }
+    //Override Constructor
+    public void Cards(String color, String rank, String symbol, int value, boolean playable){
+        this.color=color;
+        this.rank=rank;
+        this.symbol=symbol;
+        this.value=value;
+        this.playable=playable;
+    }
 
     // ACCESSORS
     public String getColor() {
         return this.color;
     }
-
-    public char getRank() {
+    public String getRank() {
         return this.rank;
     }
-
+    public String getSymbol(){
+        return this.symbol;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public boolean getPlayable(){
+        return this.playable;
+    }
+    public String getReport(){
+        String report="Color: "+ this.color + "\n"+"Rank: "+ this.rank + "\n"+"Symbol: "+ this.symbol + "\n"+"Value: "+ this.value + "\n"+"Playable: "+ this.playable + "\n"; 
+        return report;
+    }
     // MUTATORS
     public void setColor(String color) {
         this.color = color;
     }
-
-    public void setRank(char rank) {
+    public void setRank(String rank) {
         this.rank = rank;
     }
+  
 
     // METHODS
-}
+   // public static void toString(){
+
+    }
+
+/*6. Write a mutator method for Value and Playable
+9. Write an equals method that compares the current card against another card to determine if they are
+equal.*/
--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,7 @@
 public class Deck {
     // PROPERTIES
     private Card[] cards;
+    
 
     // CONSTRUCTORS
     public Deck() {}
@@ -20,3 +21,17 @@ public class Deck {
         // Shuffle this.cards in a random order
     }
 }
+/*
+10. Make a Deck Object that contains a Card array to contain all cards.
+11. Write a default constructor for your deck object that create 52 cards by default.
+12. Write a constructor that specifies the number of suits , the number of cards per suit, Boolean for
+ace high/low with low ace value=1.
+13. Make a private method called makeDeck(). That populates a class level
+variable which is an array of cards with the need fields.
+14. Make a toString()method that list the cards in the deck from top to
+bottom.
+15.Write an accessor that returns the number of cards in the deck.
+16.Write an accessor called getCardAt() that returns the Card at position
+x in the deck
+17. Write an mutator called shuffle () that shuffle/randomizes the order
+of the cards.*/
